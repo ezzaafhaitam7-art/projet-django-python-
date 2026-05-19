1e6813a08e118f29d5ad4161659c39fc88da4b5a
ezzaafhaitam7-art <ezzaafhaitam7@gmail.com>
2026-05-19 20:35:45 +0100

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
-from .models import Bien, ImageBien
+from .models import Locataire, Contrat, Paiement, Demande, Client
 
-class ImageBienInline(admin.TabularInline):
-    model = ImageBien
-    extra = 1
-    max_num = 10   
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('nom_complet', 'cin', 'telephone', 'profession')
+    search_fields = ('nom_complet', 'cin')
 
-class BienAdmin(admin.ModelAdmin):
-    inlines = [ImageBienInline]
+@admin.register(Demande)
+class DemandeAdmin(admin.ModelAdmin):
+    list_display = ('client', 'bien', 'date_demande', 'statut', 'message')
+    
+    list_editable = ('statut',)
+    
+    list_filter = ('statut', 'date_demande')
+    
+    search_fields = ('client__nom_complet', 'client__cin', 'bien__Titre')
 
-admin.site.register(Bien, BienAdmin)
-admin.site.register(ImageBien)
-# Register your models here.
+@admin.register(Locataire)
+class LocataireAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenom', 'email', 'telephone')
+
+@admin.register(Contrat)
+class ContratAdmin(admin.ModelAdmin):
+    list_display = ('bien', 'locataire', 'date_debut', 'date_fin')
+
+@admin.register(Paiement)
+class PaiementAdmin(admin.ModelAdmin):
+    list_display = ('contrat', 'montant', 'date_paiement', 'statut')
+    list_filter = ('statut',)
